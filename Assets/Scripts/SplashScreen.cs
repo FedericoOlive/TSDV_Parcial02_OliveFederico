3f4acdf72c4f09105098aa15260cd06365f4e014
Federico Olive <FedericoOlive@gmail.com>
2021-06-28 11:14:32 -0300

    Add SplashScreen.

---
+++ Assets/Scripts/SplashScreen.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashScreen : MonoBehaviour
+{
+    //private Animator anim;
+    void Start()
+    {
+        //anim = GetComponent<Animator>();
+        
+    }
+    void Update()
+    {
+        
+    }
+
+    public void FinalizeSplash()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
